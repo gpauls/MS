30565fca45e8bc8f92d0a2afa0226fb3bb229b04
Georg Pfeiffer <g.pfeiffer@diamonddogs.cc>
2015-10-22 16:40:59 +0200

    fix command discovery

--- src/MS.Infrastructure/Handling/CommandDiscovery.cs
+++ src/MS.Infrastructure/Handling/CommandDiscovery.cs
@@ -17,24 +17,21 @@ namespace MS.Infrastructure.Handling
         public void Register(ICommandRegistrar registrar)
         {
             var registrations = _container.GetCurrentRegistrations();
+            var commandHandlers = registrations.Where(r => typeof (ICommandHandler).IsAssignableFrom(r.ServiceType));
 
-            foreach (var registration in registrations)
+            foreach (var commandHandler in commandHandlers)
             {
-                if (typeof (ICommandHandler).IsAssignableFrom(registration.ServiceType))
+                var genericArguments = commandHandler.ServiceType.GetGenericArguments().ToList();
+                switch (genericArguments.Count())
                 {
-                    var genericArguments = registration.ServiceType.GetGenericArguments().ToList();
-                    if (genericArguments.Count() == 1)
-                    {
+                    case 1:
                         RegisterCommandHandler(genericArguments[0], registrar);
-                    }
-                    else if (genericArguments.Count() == 1)
-                    {
+                        break;
+                    case 2:
                         RegisterCommandResponseHandler(genericArguments[0], genericArguments[1], registrar);
-                    }
-                    else
-                    {
-                        throw new Exception("The service " + registration.ServiceType + " must by of type ICommandHandler<T> or ICommandHandler<TRequest, TResponse>");
-                    }
+                        break;
+                    default:
+                        throw new Exception("The service " + commandHandler.ServiceType + " must by of type ICommandHandler<T> or ICommandHandler<TRequest, TResponse>");
                 }
             }
         }
